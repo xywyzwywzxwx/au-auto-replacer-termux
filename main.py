c868e5eb76b8127c106a19d6573d05ba4ddcea0d
xywyzwywzxwx <63850094+xywyzwywzxwx@users.noreply.github.com>
2020-09-12 22:35:46 +0300

    Fix & update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 from filecmp import cmp
 from shutil import which, copyfile
 
-print("Running...")
+print("Script running...")
 # Check if have access to sdcard
 while True:
   if not access('/sdcard/', W_OK):
@@ -23,9 +23,9 @@ file2 = '/sdcard/Android/data/com.innersloth.spacemafia/files/secureNew'
 
 # Checking needed files and folders exists
 if not path.exists(dir2):
-  exit("Error: Not found Among Us data on path", dir2)
+  exit(f"Error: Not found Among Us data on path: {dir2}")
 
-
+print("Waiting for file changes...")
 # Replacing file 
 while True:
   system(f"touch {file2}")
